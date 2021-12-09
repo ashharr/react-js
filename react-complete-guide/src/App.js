77c56f30889833a84940016510e19acb66a768c4
ashharr <shaikh.ashhar6@gmail.com>
2021-12-09 12:02:02 +0530

    states and working with events

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense"
+
 
 function App() {
   const expenses = [
@@ -22,10 +24,16 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  }
+
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <Expenses items={expenses}/>
       </div>
   );
